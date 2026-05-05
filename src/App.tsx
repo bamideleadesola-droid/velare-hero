aa60769e13bd6d2ebe0064e305f311b168222a43
bamideleadesola-droid <280073895+bamideleadesola-droid@users.noreply.github.com>
2026-05-06 00:38:35 +0100

    Add design philosophy section

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FeaturedResidences } from "./components/FeaturedResidences";
+import { DesignPhilosophy } from "./components/DesignPhilosophy";
 import { VelareHero } from "./components/VelareHero";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <VelareHero />
       <FeaturedResidences />
+      <DesignPhilosophy />
     </>
   );
 }
